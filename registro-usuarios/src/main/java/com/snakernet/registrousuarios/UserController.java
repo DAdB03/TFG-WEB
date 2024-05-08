8381483971a65b990702858a4c12fcaf8008e229
SnakerRB <dr6887762@gmail.com>
2024-05-08 02:24:19 +0200

    retoques y funcionalidad tabla

--- registro-usuarios/src/main/java/com/snakernet/registrousuarios/UserController.java
+++ registro-usuarios/src/main/java/com/snakernet/registrousuarios/UserController.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -104,10 +107,12 @@ public class UserController {
 	}
 
 	@GetMapping("/table/list")
-	public ResponseEntity<List<User>> obtenerTodosLosUsuarios() {
-		List<User> usuarios = userService.listarTodosLosUsuarios();
-		System.out.println("Listar todos");
-		System.out.println(usuarios);
-		return ResponseEntity.ok(usuarios);
-	}
+    public ResponseEntity<List<User>> obtenerTodosLosUsuarios(
+            @RequestParam(defaultValue = "0") int page,  // Página actual, comenzando desde 0
+            @RequestParam(defaultValue = "5") int pageSize) {  // Cantidad de elementos por página
+
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<User> usuarios = userService.listarTodosLosUsuarios(pageable);
+        return ResponseEntity.ok(usuarios.getContent());
+    }
 }
--- registro-usuarios/src/main/java/com/snakernet/registrousuarios/UserService.java
+++ registro-usuarios/src/main/java/com/snakernet/registrousuarios/UserService.java
@@ -1,9 +1,10 @@
 package com.snakernet.registrousuarios;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +30,7 @@ public class UserService {
 		return userRepository.save(user); // Guarda el usuario en la base de datos
 	}
 	
-	public List<User> listarTodosLosUsuarios() {
-        return userRepository.findAll();
+	public Page<User> listarTodosLosUsuarios(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 }
